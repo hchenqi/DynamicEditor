2da76b7725ebf9422cf9c85e918ed26f583fa307
Chenqi <1002652154@qq.com>
2026-06-28 19:05:12 +0200

    ItemBlock use Holder

--- src/ItemBlock.h
+++ src/ItemBlock.h
@@ -7,45 +7,48 @@
 
 #include <ViewDesign/view/frame/MutableFrame.h>
 #include <ViewDesign/view/control/Placeholder.h>
+#include <ViewDesign/common/holder.h>
 
 
-class ItemBlock : public MutableFrame<Relative, Relative> {
+class ItemBlock : private Holder<item_block_ref>, private Holder<ItemPtr>, public MutableFrame<Relative, Relative> {
 public:
-	ItemBlock(item_block_ref ref, Meta& meta) : MutableFrame(new Placeholder<Fixed, Fixed>()), ref(std::move(ref)), root(Deserialize(meta)) {
-		Reset(new View(*this));
-	}
+	ItemBlock(const item_block_ref& ref, Meta& meta) : Holder<item_block_ref>(ref), Holder<ItemPtr>(Deserialize(meta)), MutableFrame(new View(*this)) {}
 
 private:
-	item_block_ref ref;
-	bool modified = false;
+	item_block_ref& GetRef() { return Holder<item_block_ref>::value; }
+	const item_block_ref& GetRef() const { return Holder<item_block_ref>::value; }
 private:
 	std::unique_ptr<const Item> Deserialize(Meta& meta) const {
-		if (auto data = ref.read(); data.empty()) {
+		if (auto data = GetRef().read(); data.empty()) {
 			return std::make_unique<DescriptorAny>(
 				std::make_unique<ItemDescriptor>(
 					meta.GetDescriptorRegistry(), std::make_unique<StringRef>(meta.GetStringTable().Insert(u""))
 				)
 			);
 		} else {
-			Item::DeserializeContext context(ref.get_manager(), std::move(data), meta);
+			Item::DeserializeContext context(GetRef().get_manager(), std::move(data), meta);
 			return Item::Construct(DescriptorAny::type, context);
 		}
 	}
+
+private:
+	bool modified = false;
+private:
 	void Serialize() {
-		SerializeContext context(ref.get_manager());
-		root->Serialize(context);
+		SerializeContext context(GetRef().get_manager());
+		GetRoot()->Serialize(context);
 		auto [data, ref_list] = context.Get();
 		if (data.size() > block_size_limit) {
 			throw std::logic_error("block size exceeds limit");
 		}
-		ref.write(data, ref_list);
+		GetRef().write(data, ref_list);
 	}
 
 private:
-	ItemPtr root;
+	ItemPtr& GetRoot() { return Holder<ItemPtr>::value; }
 public:
 	ItemPtr SetRoot(ItemPtr root) {
-		std::swap(this->root, root);
+		std::swap(GetRoot(), root);
 		Serialize();
 		MutableFrame::Reset(new View(*this));
 		return root;
@@ -55,14 +58,14 @@ private:
 	class View : public Item::View {
 	public:
 		View(ItemBlock& item_block) : Item::View(
-			new Item::ViewRef(*this, item_block.root->GetView())
+			new Item::ViewRef(*this, item_block.GetRoot()->GetView())
 		), item_block(item_block) {}
 	private:
 		ItemBlock& item_block;
 	private:
 		virtual void OnChildUpdate(const Item::View& child, std::unique_ptr<const Item> item) const override {
 			ItemBlock& item_block = this->item_block;
-			GetHistory().OnItemBlockUpdate(item_block.ref, item_block.SetRoot(std::move(item)));
+			GetHistory().OnItemBlockUpdate(item_block.GetRef(), item_block.SetRoot(std::move(item)));
 		}
 	};
 };
